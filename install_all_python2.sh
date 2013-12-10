93fc6de760683e077662af673da73fada8344550
hesperus22 <przemko@przemko-robakowski.pl>
2013-12-10 16:07:40 +0100

    Fixed adress of install script

--- install_all_python2.sh
+++ install_all_python2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://raw.github.com/hesperus22/WebIOPi/master/install_all.sh
+wget https://raw.github.com/hesperus22/InteligentneSystemyBudynkow/master/install_all.sh
 chmod 755 install_all.sh
 ./install_all.sh python
