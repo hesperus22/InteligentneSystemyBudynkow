8e4cb05320b3ff038e8258c34bcb01519facb8db
hesperus22 <przemko@przemko-robakowski.pl>
2013-12-10 15:37:10 +0100

    Lirc config

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt-get update
+
+#install required software
 apt-get install -y make
 apt-get install -y gcc
 apt-get install -y git
@@ -15,11 +17,6 @@ make check
 make install
 cd ..
 
-#lirc
-apt-get install -y lirc-rpi
-echo "lirc-dev" >> /etc/modules
-echo "lirc-rpi gpio_input_pin = 22" >> /etc/modules
-
 #clone repositories
 git clone https://github.com/hesperus22/InteligentneSystemyBudynkow.git InteligentneSystemyBudynkow
 git clone https://github.com/hesperus22/Adafruit-Raspberry-Pi-Python-Code.git Adafruit
@@ -34,5 +31,15 @@ cd ../..
 cd WEbIOPi
 ./setup.sh
 cd ..
-
 cp InteligentneSystemyBudynkow/config /etc/webiopi/config
+
+#lirc
+apt-get install -y lirc-rpi
+echo "lirc_dev" >> /etc/modules
+echo "lirc_rpi gpio_in_pin = 22" >> /etc/modules
+cp InteligentneSystemyBudynkow/lirc_hardware.conf /etc/lirc/hardware.conf
+cp InteligentneSystemyBudynkow/remote.conf /etc/lirc/lircd.conf
+modprobe lirc_dev
+modprobe lirc_rpi gpio_in_pin = 22
+/etc/init.d/lirc stop
+/etc/init.d/lirc start
